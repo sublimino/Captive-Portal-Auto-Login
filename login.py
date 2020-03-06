ab8d45f8ced4f26b963467c936bc790242e82fa8
Andrew Martin <sublimino@gmail.com>
2020-03-06 10:36:54 +0000

    feat: add configurable elements and password

--- login.py
+++ login.py
@@ -7,19 +7,27 @@ from selenium import webdriver
 driver = webdriver.Firefox()
 
 URL = os.getenv('PORTAL_URL')
-USER = os.environ.get('PORTAL_USER')
+USER = os.getenv('PORTAL_USER')
+USER_ELEMENT = os.getenv('PORTAL_USER_ELEMENT_NAME', "username")
+PASS = os.getenv('PORTAL_PASS', '')
+PASS_ELEMENT = os.getenv('PORTAL_PASS_ELEMENT_NAME', "password")
+SUBMIT_ELEMENT_ID = os.getenv('PORTAL_SUBMIT_ELEMENT_ID')
 
 try:
     driver.get(URL)
 except:
 	sys.exit(0)
 
-username = driver.find_element_by_name("username")
+username = driver.find_element_by_name(USER_ELEMENT)
 username.clear()
-
 username.send_keys(USER)
 
-driver.find_element_by_id("submitbutton").click()
+if PASS != "":
+    password = driver.find_element_by_name(PASS_ELEMENT)
+    password.clear()
+    password.send_keys(PASS)
+
+driver.find_element_by_id(SUBMIT_ELEMENT_ID).click()
 
 print("Logged In.")
 
